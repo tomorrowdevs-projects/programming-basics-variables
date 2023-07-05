cc91813326fd886213e5d676ef211e702148f168
rosario <rosariospalla.job@gmail.com>
2023-07-05 18:34:02 +0200

    add a comment about the problem

--- projects/001-area-of-a-room/js/index.js
+++ projects/001-area-of-a-room/js/index.js
@@ -1,1 +1,10 @@
-console.log('aaa')
+/* 
+    1. Write a program that asks the user to enter the width and length of a room.  
+
+    2. Once these values have been read, your program should compute and display the area of the room.
+
+    3. The length and the width will be entered as **floating-point numbers**.
+
+    4. Include units in your prompt and output message; either feet or meters, depending on which
+    unit you are more comfortable working with.
+*/
