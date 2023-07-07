effd9052c7da8c5d0e73bea43c470af9ecf82b2d
rosario <rosariospalla.job@gmail.com>
2023-07-07 23:13:13 +0200

    Creata a .js programm to solve the problem

--- projects/001-area-of-a-room/js/index.js
+++ projects/001-area-of-a-room/js/index.js
@@ -9,4 +9,27 @@
     unit you are more comfortable working with.
 */
 
-console.log("aaa")
+let width = prompt("Inserisci la largezza della stanza in m: ");
+alert("Larghezza inserita= " + width)
+width = parseFloat(width); // Convert the string type in a floating number type
+// alert("Il tipo di dato inserito è: " + typeof width); // Check the rightness of the convertion
+
+let length = prompt("Inserisci la lunghezza della stanza in m: ");
+alert("Lunghezza inserita= " + length)
+length = parseFloat(length); // Convert the string type in a floating number type
+// alert("Il tipo di dato inserito è: " + typeof width); // Check the rightness of the convertion
+
+
+// Added a control to be sure that the user input a positive number both integer or float
+if ( width > 0 && length > 0 ) {
+
+    const area = ((width * 10) * (length* 10)) / 100; // Avoid the approssimations errors
+
+    alert("Area della stanza: " + area + "mq");
+
+} else {
+
+    alert("ERRORE! SONO AMMESSI SOLO NUMERI >0 ! \nAggiorna la pagina web e inserisci un valore numerico quando richiesto!");
+
+}
+
