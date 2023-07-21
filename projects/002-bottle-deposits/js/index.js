0af472228d2ccfb9672f1f2620e31759ec41d78e
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-07-21 21:40:11 +0200

    Two variables to ask how many bottles

--- projects/002-bottle-deposits/js/index.js
+++ projects/002-bottle-deposits/js/index.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// 1) Prompt to ask how many bottles have been entered.
+
+const smallDrink = parseFloat(
+    prompt(
+        'How many bottles equal to or less than one litre did you put in the drink container?'
+    )
+);
+const bigDrink = parseFloat(
+    prompt(
+        'How many bottles larger than one litre did you put in the drink container'
+    )
+);
+
+// Calculate total refund and add toFixed(2) to get two decimal numbers
+const totalRefundDrink = alert(
+    `The total refund is $${(smallDrink * 0.1 + bigDrink * 0.25).toFixed(2)}.`
+);
