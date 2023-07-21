baf24fd071b0376037cc1c6881d79bbf544a0ef1
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-07-21 17:34:12 +0200

    Inserted comments and exercise

--- projects/001-area-of-a-room/js/index.js
+++ projects/001-area-of-a-room/js/index.js
@@ -1,3 +1,15 @@
-console.log('aaa');
+'use strict';
 
-console.log('bbb');
+// Prompt gives back a string. I inserted a prompt with parseFloat
+let widthRoom = parseFloat(
+    prompt('Enter the width in cm of your room, please')
+);
+
+let lengthRoom = parseFloat(
+    prompt('Enter the length in cm of your room, please')
+);
+
+// Without a parseFloat() result NaN
+let areaRoom = alert(
+    `The area of your room is ${widthRoom * lengthRoom} square centimetres.`
+);
