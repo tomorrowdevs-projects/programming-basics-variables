e88ab8c04138e3db563be53620b57dbcfeb6334c
Ale Furneri <ale.furneri93@gmail.com>
2023-09-23 19:58:42 +0200

    solution007

--- projects/007-day-old-bread/js/index.js
+++ projects/007-day-old-bread/js/index.js
@@ -1,3 +1,12 @@
-fgfgfggit 
-console,log("ale")
-onsole,log("ale")
+const breadPrice = 3.49; // Regular price for loavesBread
+
+const numLoavesBread = prompt("Insert how many Loaves Bread");
+
+let totFullPrice =(numLoavesBread * breadPrice).toFixed(2);
+let percentDiscount = (totFullPrice * 60) / 100; //discount = 60%;
+let discountPrice = (totFullPrice - percentDiscount);
+
+console.log(`Total Pices of Bread n${numLoavesBread}`);
+console.log(`Regular Price: ${totFullPrice}€`);
+console.log(`Total Discount(60% OFF): -${percentDiscount.toFixed(2)}€`);
+console.log(`Final Price: ${discountPrice.toFixed(2)}€`)
