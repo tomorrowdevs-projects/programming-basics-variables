8cfe39f58381044c53a79276ab2129bf3a68006d
Ale Furneri <ale.furneri93@gmail.com>
2023-09-23 18:19:49 +0200

    solution006

--- projects/006-sum-of-digits-in-a-integer/js/index.js
+++ projects/006-sum-of-digits-in-a-integer/js/index.js
@@ -1,1 +1,14 @@
-console.log("novo commit di prova")
+const fourDigit = prompt("Insert the 4-digit Number");
+
+let firstNum = Math.floor(fourDigit / 1000);
+let secondNum = Math.floor((fourDigit % 1000) / 100);
+let tirdNumber = Math.floor((fourDigit % 100) / 10 );
+let lastNumber = fourDigit % 10;
+//console.log(firstNum);
+//console.log(secondNum);
+//console.log(tirdNumber);
+//console.log(lastNumber);
+
+const sumFourDigit = firstNum + secondNum + tirdNumber +lastNumber;
+
+console.log(sumFourDigit); 
