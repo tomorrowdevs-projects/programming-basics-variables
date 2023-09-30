008b2efd1ac7c1e9b0ef55886de1212aa5e585a9
saratemwhy <sara.tasso.emone@gmail.com>
2023-09-30 20:55:54 +0200

    solution m1-004

--- projects/004-units-of-time/js/index.js
+++ projects/004-units-of-time/js/index.js
@@ -1,1 +1,15 @@
-console.log('ciao');
+let ask_days = prompt("Inserisci i giorni: ");
+let ask_hours = prompt("Inserisci le ore: ");
+let ask_minutes = prompt("Inserisci i minuti: ");
+let ask_seconds = prompt("Inserisci i secondi: ");
+
+
+function resultSeconds (ask_days, ask_hours, ask_minutes, ask_seconds) {
+    return((ask_days*86400)+(ask_hours*3600)+(ask_minutes*60)+(ask_seconds*1));
+}
+
+
+
+console.log(resultSeconds(ask_days, ask_hours, ask_minutes, ask_seconds));
+
+
