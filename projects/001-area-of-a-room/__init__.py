bf80a57e7c164fd2b76472e055dce6d65dcb625c
Roberto <robertomoggia95@gmail.com>
2024-03-17 17:00:59 +0100

    Solution of the first exercise

---
+++ projects/001-area-of-a-room/__init__.py
@@ -0,0 +1,12 @@
+#RICHIESTA
+# Write a program that:
+#1) asks the user to enter the width and length of a room;  
+#2) Once these values have been read, your program should compute and display the area of the room; 
+#3) The length and the width will be entered as **floating-point numbers**;  
+#4) Include units in your prompt and output message; either feet or meters, depending on which unit you are more comfortable working with.
+
+unit_measure=input("Enter the abbreviaton of the S.I. unit of measure you want to use:")
+room_legnth=float(input("Enter the length of the room in the unit of measur you choosed (the length has to be insert in digits):"));
+room_width=float(input("Enter the width of the room in the unit of measur you choosed (the length has to be insert in digits):"));
+room_area=room_legnth*room_width;
+print("The area of the room is "+str(room_area)+' '+unit_measure+'\xb2')
