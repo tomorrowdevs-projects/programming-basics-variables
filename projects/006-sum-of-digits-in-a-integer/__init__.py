c743d3236b467549f78b31805a7282613a751c70
Roberto <robertomoggia95@gmail.com>
2024-03-19 23:20:19 +0100

    updated solution/m1-006

--- projects/006-sum-of-digits-in-a-integer/__init__.py
+++ projects/006-sum-of-digits-in-a-integer/__init__.py
@@ -3,9 +3,9 @@
 # 2) displays the sum of its digits.
 
 number=input("Please, insert a four digits number:")
-n1=number[slice(1)]
-n2=number[slice(1,2,1)]
-n3=number[slice(2,3,1)]
-n4=number[slice(3,4,1)]
+n1=number[0]
+n2=number[1]
+n3=number[2]
+n4=number[3]
 sum=int(n1)+int(n2)+int(n3)+int(n4)
 print(n1+' + '+n2+' + '+n3+' + '+n4+' = '+str(sum))
