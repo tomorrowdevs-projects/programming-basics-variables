d51914442881fe8232309d24174a3397c51222be
matteorr88 <m.matysiak97@gmail.com>
2024-04-12 17:00:04 +0200

    added the solution to 001-area-of-a-room

--- projects/001-area-of-a-room/python/main.py
+++ projects/001-area-of-a-room/python/main.py
@@ -1,3 +1,3 @@
 width = float(input("enter width in meters"))
-length = float(input("enter lenght meters"))
+length = float(input("enter lenght in meters"))
 print("the area of your room is", width * length, "meters")
