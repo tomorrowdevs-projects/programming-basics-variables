8ed763cefc855dd75b37f616cf1a303c9ab93d58
Natalia <natalia.muccio@gmail.com>
2024-06-16 16:41:33 +0200

    solution/m1-003

--- projects/003-making-change/python/main.py
+++ projects/003-making-change/python/main.py
@@ -1,45 +1,21 @@
 coin = int(input("Inserisci un numero di centesimi: "))
-print(coin, "cents =", end= " ")
 
-if (coin>=200):
-    print((coin//200), "toonies", end="")
-    coin = coin%200
-    if (coin==0):
-        print(".")
-    else:
-        print(", ", end="")
+toonies = coin//200
+coin = coin%200
 
-if (coin>=100):
-    print((coin//100), "loonies", end="")
-    coin = coin%100
-    if (coin==0):
-        print(".")
-    else:
-        print(", ", end="")
+loonies = coin//100
+coin = coin%100
 
-if (coin>=25):
-    print((coin//25), "quarters", end="")
-    coin = coin%25
-    if (coin==0):
-        print(".")
-    else:
-        print(", ", end="") 
+quarters = coin//25
+coin = coin%25
 
-if (coin>=10):
-    print((coin//10), "dimes", end="")
-    coin = coin%10
-    if (coin==0):
-        print(".")
-    else:
-        print(", ", end="") 
+dimes = coin//10
+coin = coin%10
 
-if (coin>=5):
-    print((coin//5), "nickels", end="")
-    coin = coin%5
-    if (coin==0):
-        print(".")
-    else:
-        print(", ", end="") 
+nickels = coin//5
+coin = coin%5
 
-if (coin>=1):
-    print((coin//1), "pennies.")
+pennies = coin//1
+coin = coin%1
+
+print(f"{toonies} toonies, {loonies} loonies, {quarters} quarters, {dimes} dimes, {nickels} nickels, {pennies} pennies.")
