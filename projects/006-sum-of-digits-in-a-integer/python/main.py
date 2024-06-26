7378a9c051b50c2a37b6890cbba896f85843ad2c
sommailaria <mail@ilariasomma.io>
2024-06-26 22:00:28 +0200

    Solution to "Sum of digits in an integer "

---
+++ projects/006-sum-of-digits-in-a-integer/python/main.py
@@ -0,0 +1,7 @@
+integer_from_user = input("Enter a four digits integer: ")
+first_digit = int(integer_from_user[0])
+second_digit = int(integer_from_user[1])
+third_digit = int(integer_from_user[2])
+fourth_digit = int(integer_from_user[3])
+sum_of_digits = first_digit + second_digit + third_digit + fourth_digit
+print(sum_of_digits)
